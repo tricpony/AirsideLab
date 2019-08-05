3a056dd05575a1b5e1c37f66e681332aaf8b092f
Brian Arthur <tricpony@mac.com>
2019-08-05 09:15:23 -0400

    Initial Commit

---
+++ AirsideLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirsideLab
+//
+//  Created by aarthur on 8/5/19.
+//  Copyright © 2019 Gigabit LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
