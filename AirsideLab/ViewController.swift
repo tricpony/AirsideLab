491ad15dd012302f2aaa23577626b0b73ea2daaa
Brian Arthur <tricpony@mac.com>
2019-08-05 10:02:03 -0400

    original commit

--- AirsideLab/ViewController.swift
+++ AirsideLab/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        inputTextField.text = "P<USARICHARDS<STEVENS<JR<<GEORGE<MICHAEL<<<<"
     }
 
+    @IBAction func parseTapped(_ sender: Any) {
+        guard let input = inputTextField.text else { return }
+        outputLabel.text = parseMRZ(input)
+    }
 
+    func parseMRZ(_ mrz: String) -> String? {
+        let index = mrz.index(mrz.startIndex, offsetBy: 5)
+        let subMrz = mrz.substring(from: index)
+        let lastFirstNames = subMrz.components(separatedBy: "<<")
+        guard var lastname = lastFirstNames.first else { return nil }
+        var firstname = lastFirstNames[1]
+
+        firstname = firstname.replacingOccurrences(of: "<", with: " ")
+        lastname = lastname.replacingOccurrences(of: "<", with: " ")
+
+        return firstname + " " + lastname
+    }
 }
 
